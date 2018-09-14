13619fc5cd082f40cefe454752e01a7bb127479d
Adeola Adebayo <adeola.salvador@gmail.com>
2018-09-14 16:30:06 +0100

    Added Wallets and Transactions

--- src/Block.java
+++ src/Block.java
@@ -1,16 +1,17 @@
+import java.util.ArrayList;
 import java.util.Date;
 
 class Block {
 
     String hash;
     String previousHash;
-    private String data;
+    public String merkleRoot;
+    public ArrayList<Transaction> transactions = new ArrayList<>();
     private long timeStamp;
     private int nonce;
 
-    Block(String data, String previousHash) {
+    Block(String previousHash) {
         this.previousHash = previousHash;
-        this.data = data;
         this.timeStamp = new Date().getTime();
         this.hash = calculateHash();
     }
@@ -20,17 +21,31 @@ class Block {
                 previousHash +
                         Long.toString(timeStamp) +
                         Integer.toString(nonce) +
-                        data
+                        merkleRoot
         );
     }
 
     public void mineBlock(int difficulty){
-        String target = new String(new char [difficulty]).replace('\0','0');
+        merkleRoot = StringUtil.getMerkleRoot(transactions);
+        String target = StringUtil.getDifficultyString(difficulty);
         while(!hash.substring(0, difficulty).equals(target)){
             nonce++;
             hash = calculateHash();
         }
         System.out.println("Block mined! : " + hash);
+    }
+
+    public boolean addTransaction(Transaction transaction) {
+        if (transaction == null) return false;
+        if ((previousHash != "0")) {
+            if ((transaction.processTransaction() != true)) {
+                System.out.println("Transaction failed to process. Discarded");
+                return false;
+            }
+        }
+        transactions.add(transaction);
+        System.out.println("Transaction successfully added to block");
+        return true;
     }
 }
 
